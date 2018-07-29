1f0cc09529a9feba22f1fd81c73ba856d0b1edb4
Jan WALSH <jwalsh@e3r11p22.42.fr>
2018-07-29 17:14:56 +0200

    quick md5 implementation

--- inc/ft_md5.h
+++ inc/ft_md5.h
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/26 14:43:42 by jwalsh            #+#    #+#             */
-/*   Updated: 2018/07/28 16:23:50 by jwalsh           ###   ########.fr       */
+/*   Updated: 2018/07/29 15:52:23 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,14 @@ typedef	struct	s_md5_state
 	unsigned char	buf[BUFFER_SIZE * 2 + 1];
 	int				ret;
 	uint64_t		length;
+	// or uint32_t length[2];
 }				t_md5_state;
 
 void			ft_md5(t_task *task);
 t_md5_state		*md5_init_state(void);
 void			md5_update_state(t_md5_state *state);
 t_md5_state		*md5_pad(t_md5_state *state);
+void			md5_transform(t_md5_state *state);
 
 
 // helper functions
@@ -41,15 +43,159 @@ uint32_t	md5_f(uint32_t x, uint32_t y, uint32_t z);
 uint32_t	md5_g(uint32_t x, uint32_t y, uint32_t z);
 uint32_t	md5_h(uint32_t x, uint32_t y, uint32_t z);
 uint32_t	md5_i(uint32_t x, uint32_t y, uint32_t z);
+uint32_t	rotate_left(uint32_t x, uint32_t n);
 
-// TODO: may need 64
-uint32_t				g_md5_t[64];
 
-static const uint32_t	g_md5_s[4][4] = {
-	{7, 12, 17, 22},
-	{5, 9, 14, 20},
-	{4, 11, 16, 23},
-	{6, 10, 15, 21}
+// void	encode(unsigned char *output, uint32_t *input, unsigned int len);
+// void	decode(uint32_t *output, unsigned char *input, unsigned int len);
+
+// original
+static const uint32_t		g_md5_t[64] = {
+	0xd76aa478,
+	0xe8c7b756,
+	0x242070db,
+	0xc1bdceee,
+	0xf57c0faf,
+	0x4787c62a,
+	0xa8304613,
+	0xfd469501,
+	0x698098d8,
+	0x8b44f7af,
+	0xffff5bb1,
+	0x895cd7be,
+	0x6b901122,
+	0xfd987193,
+	0xa679438e,
+	0x49b40821,
+	0xf61e2562,
+	0xc040b340,
+	0x265e5a51,
+	0xe9b6c7aa,
+	0xd62f105d,
+	0x2441453,
+	0xd8a1e681,
+	0xe7d3fbc8,
+	0x21e1cde6,
+	0xc33707d6,
+	0xf4d50d87,
+	0x455a14ed,
+	0xa9e3e905,
+	0xfcefa3f8,
+	0x676f02d9,
+	0x8d2a4c8a,
+	0xfffa3942,
+	0x8771f681,
+	0x6d9d6122,
+	0xfde5380c,
+	0xa4beea44,
+	0x4bdecfa9,
+	0xf6bb4b60,
+	0xbebfbc70,
+	0x289b7ec6,
+	0xeaa127fa,
+	0xd4ef3085,
+	0x4881d05,
+	0xd9d4d039,
+	0xe6db99e5,
+	0x1fa27cf8,
+	0xc4ac5665,
+	0xf4292244,
+	0x432aff97,
+	0xab9423a7,
+	0xfc93a039,
+	0x655b59c3,
+	0x8f0ccc92,
+	0xffeff47d,
+	0x85845dd1,
+	0x6fa87e4f,
+	0xfe2ce6e0,
+	0xa3014314,
+	0x4e0811a1,
+	0xf7537e82,
+	0xbd3af235,
+	0x2ad7d2bb,
+	0xeb86d391
+};
+
+// static const uint32_t		g_md5_t[64] = {
+// 	0x78a46ad7,
+// 	0x56b7c7e8,
+// 	0xdb702024,
+// 	0xeecebdc1,
+// 	0xaf0f7cf5,
+// 	0x2ac68747,
+// 	0x134630a8,
+// 	0x019546fd,
+// 	0xd8988069,
+// 	0xaff7448b,
+// 	0xb15bffff,
+// 	0xbed75c89,
+// 	0x2211906b,
+// 	0x937198fd,
+// 	0x8e4379a6,
+// 	0x2108b449,
+// 	0x62251ef6,
+// 	0x40b340c0,
+// 	0x515a5e26,
+// 	0xaac7b6e9,
+// 	0x5d102fd6,
+// 	0x53144402,
+// 	0x81e6a1d8,
+// 	0xc8fbd3e7,
+// 	0xe6cde121,
+// 	0xd60737c3,
+// 	0x870dd5f4,
+// 	0xed145a45,
+// 	0x05e9e3a9,
+// 	0xf8a3effc,
+// 	0xd9026f67,
+// 	0x8a4c2a8d,
+// 	0x4239faff,
+// 	0x81f67187,
+// 	0x22619d6d,
+// 	0x0c38e5fd,
+// 	0x44eabea4,
+// 	0xa9cfde4b,
+// 	0x604bbbf6,
+// 	0x70bcbfbe,
+// 	0xc67e9b28,
+// 	0xfa27a1ea,
+// 	0x8530efd4,
+// 	0x051d8804,
+// 	0x39d0d4d9,
+// 	0xe599dbe6,
+// 	0xf87ca21f,
+// 	0x6556acc4,
+// 	0x442229f4,
+// 	0x97ff2a43,
+// 	0xa72394ab,
+// 	0x39a093fc,
+// 	0xc3595b65,
+// 	0x92cc0c8f,
+// 	0x7df4efff,
+// 	0xd15d8485,
+// 	0x4f7ea86f,
+// 	0xe0e62cfe,
+// 	0x144301a3,
+// 	0xa111084e,
+// 	0x827e53f7,
+// 	0x35f23abd,
+// 	0xbbd2d72a,
+// 	0x91d386eb
+// };
+
+// static const uint32_t	g_md5_s[4][4] = {
+// 	{7, 12, 17, 22},
+// 	{5, 9, 14, 20},
+// 	{4, 11, 16, 23},
+// 	{6, 10, 15, 21}
+// };
+
+static const uint32_t	g_md5_s[16] = {
+	7, 12, 17, 22,
+	5, 9, 14, 20,
+	4, 11, 16, 23,
+	6, 10, 15, 21
 };
 
 #endif
--- src/md5/ft_md5.c
+++ src/md5/ft_md5.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 16:46:57 by jwalsh            #+#    #+#             */
-/*   Updated: 2018/07/28 16:23:29 by jwalsh           ###   ########.fr       */
+/*   Updated: 2018/07/29 17:07:33 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,19 @@ void	ft_md5(t_task *task)
 	if (!(state = md5_init_state()))
 		return ;
 	
-	
-	
 	// A. read from file
 	if (!task->file)
 		return ;
 	// TODO: check for opening error
 	fd = open(task->file, O_RDONLY);
 
-	// read [16 * 32 bits] / [16 * 4] at a time 
-	// for each full block
-	// int ret;
 	// TODO: use return value to check for errors
 
+	hex_dump("first state", state->state, 16);
 	while ((state->ret = read(fd, &state->buf, BUFFER_SIZE - 1)) == BUFFER_SIZE - 1)
 		md5_update_state(state);
 	md5_update_state(md5_pad(state));
-
+	hex_dump("final state", state->state, 16);
 	// print result
 }
 
@@ -49,6 +45,8 @@ void	md5_update_state(t_md5_state *state)
 	printf("md5_update_state\n");
 	state->length += state->ret;
 	(void)state;
+
+	md5_transform(state);
 }
 
 t_md5_state	*md5_pad(t_md5_state *state)
@@ -57,64 +55,80 @@ t_md5_state	*md5_pad(t_md5_state *state)
 	// uint8_t		*p;
 
 	printf("md5_pad\n");
+	hex_dump("buffer before padding", state->buf, 64);
 	state->length += state->ret;
-	printf("length: %llu\n", state->length);
-	printf("ret: %d\n", state->ret);
-	hex_dump("buffer", state->buf, state->ret);
+	// printf("length: %llu\n", state->length);
+	// printf("ret: %d\n", state->ret);
+	// hex_dump("buffer", state->buf, state->ret);
 	// pad state->buf with 1 and 0's until length(state->buf) % (512/8) == 448/8
 	state->buf[state->ret] = 0x80;
-	hex_dump("buffer", state->buf, state->ret + 1);
+	// hex_dump("buffer", state->buf, state->ret + 1);
 	if ((state->ret + 1) % 64 < 56)
 	{
+		printf("length less than 56\n");
 		tmp = 56 - (state->length % 64);
 		ft_memset(&state->buf[state->ret + 1], 0, tmp);
 	}
 	else
 	{
+		printf("length more than 56\n");
 		tmp = 64 + 56 - (state->length % 64);
 		ft_memset(&state->buf[state->ret], 0, tmp);
 	}
-	int i = 0;
+	// int i = 0;
 
 	// then add total length of message (before padding) as unint64_t
-	while (i < 8)
-	{
-		// printf("state->length >> %d*8: %llx\n", i, state->length >> (i*8));
-		state->buf[state->ret + tmp + 7 - i] = (uint8_t)(state->length >> (i*8));
-		++i;
-	}
-	hex_dump("buffer", state->buf, state->ret + tmp + 8);
-	printf("final size: %lu\n", state->ret + tmp + 8);
+	uint64_t length = state->length * 8;
+	char	*lp = (char *)&length;
+	// while (i < 4)
+	// {
+	// 	// printf("state->length >> %d*8: %llx\n", i, state->length >> (i*8));
+	// 	state->buf[state->ret + tmp + 3 - i] = (uint8_t)(length >> (i*8));
+	// 	++i;
+	// }
+	hex_dump("length", &length, 8);
+	state->buf[state->ret + tmp + 0] = lp[0];
+	state->buf[state->ret + tmp + 1] = lp[1];
+	state->buf[state->ret + tmp + 2] = lp[2];
+	state->buf[state->ret + tmp + 3] = lp[3];
+
+	state->buf[state->ret + tmp + 4] = lp[4];
+	state->buf[state->ret + tmp + 5] = lp[5];
+	state->buf[state->ret + tmp + 6] = lp[6];
+	state->buf[state->ret + tmp + 7] = lp[7];
+	printf("length: %llu\n", length);
+
+	hex_dump("state->state[0]", &state->state[0], 4);
+	// printf("final size: %lu\n", state->ret + tmp + 8);
 	state->ret = 0;
+	hex_dump("buffer after padding", state->buf, 64);
 	return (state);
 }
 
 t_md5_state		*md5_init_state(void)
 {
+	printf("md5_init_state\n");
 	t_md5_state *state;
-	int i;
 
 	if (!(state = (t_md5_state *)ft_memalloc(sizeof(t_md5_state))))
 		return (NULL);
-	state->state[0] = 0x67452301;
-	state->state[1] = 0xefcdab89;
-	state->state[2] = 0x98badcfe;
-	state->state[3] = 0x10325476;
-	i = 0;
-	while (i < 64)
-	{
-		g_md5_t[i] = 4294967296 * fabs(sin(i));
-		// printf("g_md5_t[%d] = %x\n", i, g_md5_t[i]);
-		++i;
-	}
+	state->state[0] = 0x01234567;
+	state->state[1] = 0x89abcdef;
+	state->state[2] = 0xfedcba98;
+	state->state[3] = 0x76543210;
+	// original
+	// state->state[0] = 0x67452301;
+	// state->state[1] = 0xefcdab89;
+	// state->state[2] = 0x98badcfe;
+	// state->state[3] = 0x10325476;
 	ft_bzero(state->buf, BUFFER_SIZE);
+	hex_dump("init state", state->state, 16);
 	return (state);
 }
 
 /*
 ** MD5 helper functions
 */
-
 
 uint32_t	md5_f(uint32_t x, uint32_t y, uint32_t z)
 {
@@ -136,22 +150,145 @@ uint32_t	md5_i(uint32_t x, uint32_t y, uint32_t z)
 	return (y ^ (x | ~z));
 }
 
+uint32_t	rotate_left(uint32_t x, uint32_t n)
+{
+	return (((x) << (n)) | ((x) >> (32-(n))));
+}
+
+
 // #define FF(a, b, c, d, x, s, ac) { \
 //  (a) += F ((b), (c), (d)) + (x) + (UINT4)(ac); \
 //  (a) = ROTATE_LEFT ((a), (s)); \
 //  (a) += (b); \
 //   }
+// #define GG(a, b, c, d, x, s, ac) { \
+//  (a) += G ((b), (c), (d)) + (x) + (UINT4)(ac); \
+//  (a) = ROTATE_LEFT ((a), (s)); \
+//  (a) += (b); \
+//   }
+// #define HH(a, b, c, d, x, s, ac) { \
+//  (a) += H ((b), (c), (d)) + (x) + (UINT4)(ac); \
+//  (a) = ROTATE_LEFT ((a), (s)); \
+//  (a) += (b); \
+//   }
+// #define II(a, b, c, d, x, s, ac) { \
+//  (a) += I ((b), (c), (d)) + (x) + (UINT4)(ac); \
+//  (a) = ROTATE_LEFT ((a), (s)); \
+//  (a) += (b); \
+//   }
 
-// uint32_t	md5_ff(uint32_t a, )
+void	md5_transform(t_md5_state *state)
+{
+	printf("md5_transform\n");
+	uint32_t	state_copy[4];
+	// uint32_t	x[16];
+	uint32_t	i;
+	uint32_t	f;
+	uint32_t	g;
 
-// ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+	state_copy[0] = state->state[0];
+	state_copy[1] = state->state[1];
+	state_copy[2] = state->state[2];
+	state_copy[3] = state->state[3];
 
-// uint32_t	rotate_left(uint32_t x, uint32_t n)
+	// for (int y = 0; y < 16; y++)
+	// {
+	// 	printf("[%d]: %d\n", y, *(((int*)(state->buf)) + y) );
+	// }
+	i = 0;
+	while (i < 64)
+	{
+		if (i == 0)
+		{
+			printf("A = %u\n", state_copy[0]);
+			printf("B = %u\n", state_copy[1]);
+			printf("C = %u\n", state_copy[2]);
+			printf("D = %u\n", state_copy[3]);
+		}
+		if (i <= 15)
+		{
+			f = md5_f(state_copy[1], state_copy[2], state_copy[3]);
+			g = i;
+		}
+		else if (16 <= i && i <= 31)
+		{
+			f = md5_g(state_copy[1], state_copy[2], state_copy[3]);
+			g = (5 * i + 1) % 16;
+		}
+		else if (32 <= i && i <= 47)
+		{
+			f = md5_h(state_copy[1], state_copy[2], state_copy[3]);
+			g = (3 * i + 5) % 16;
+		}
+		else if (48 <= i && i <= 63)
+		{
+			f = md5_i(state_copy[1], state_copy[2], state_copy[3]);
+			g = (7 * i) % 16;
+		}
+		f = f + state_copy[0] + g_md5_t[i] + state->buf[g];
+		if (i == 0)
+		{
+			printf("A = %u\n", state_copy[0]);
+			printf("B = %u\n", state_copy[1]);
+			printf("C = %u\n", state_copy[2]);
+			printf("D = %u\n", state_copy[3]);
+		}
+		// uint32_t	tmp = state_copy[3]
+		state_copy[0] = state_copy[3];
+		state_copy[3] = state_copy[2];
+		state_copy[2] = state_copy[1];
+		state_copy[1] = state_copy[1] + rotate_left(f, g_md5_s[i]);
+		// hex_dump("state", state_copy, 16);
+		// printf("sizeof(state_copy): %lu\n", sizeof(state_copy));
+		if (i == 0)
+		{
+			printf("A = %u\n", state_copy[0]);
+			printf("B = %u\n", state_copy[1]);
+			printf("C = %u\n", state_copy[2]);
+			printf("D = %u\n", state_copy[3]);
+		}
+		i++;
+	}
+	state->state[0] += state_copy[0];
+	state->state[1] += state_copy[1];
+	state->state[2] += state_copy[2];
+	state->state[3] += state_copy[3];
+}
+
+/* Encodes input (uint32_t) into output (unsigned char). Assumes len is
+  a multiple of 4.
+ */
+// void	encode(unsigned char *output, uint32_t *input, unsigned int len)
 // {
-		
+// 	unsigned int	i;
+// 	unsigned int	j;
+
+// 	i = 0;
+// 	j = 0;
+// 	while (j < len)
+// 	{
+// 		output[j] = (unsigned char)(input[i] & 0xff);
+// 		output[j+1] = (unsigned char)((input[i] >> 8) & 0xff);
+// 		output[j+2] = (unsigned char)((input[i] >> 16) & 0xff);
+// 		output[j+3] = (unsigned char)((input[i] >> 24) & 0xff);
+// 		i++;
+// 		j += 4;
+// 	}
 // }
 
-
-
-
-
+/* Decodes input (unsigned char) into output (uint32_t). Assumes len is
+  a multiple of 4.
+ */
+void	decode(uint32_t *output, unsigned char *input, unsigned int len)
+{
+	unsigned int	i;
+	unsigned int	j;
+	
+	i = 0;
+	j = 0;
+	while (j < len)
+	{
+ 		output[i] = ((uint32_t)input[j]) | (((uint32_t)input[j + 1]) << 8) |
+  			(((uint32_t)input[j + 2]) << 16) | (((uint32_t)input[j + 3]) << 24);
+	}
+}
